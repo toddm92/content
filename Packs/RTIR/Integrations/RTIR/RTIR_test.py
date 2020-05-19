abe484f51aa3cb6754eb2abd85bdd4e7e41e4e29
halpert <haplert@paloaltonetworks.com>
2020-05-19 13:03:51 +0300

    unittests for the bug fix

--- Packs/RTIR/Integrations/RTIR/RTIR_test.py
+++ Packs/RTIR/Integrations/RTIR/RTIR_test.py
@@ -32,3 +32,38 @@ def test_query_formatting(mocker):
     from RTIR import build_search_query
     query = build_search_query()
     assert not (query.endswith('+OR+') or query.endswith('+AND+'))
+
+
+RAW_HISTORY = """    
+RT/4.4.2 200 Ok
+
+# 24/24 (id/80/total)
+
+id: 80
+Ticket: 5
+TimeTaken: 0
+Type: Create
+Field:
+OldValue:
+NewValue: some new value
+Data:
+Description: Ticket created by root
+Content: Some
+Multi line
+Content
+Creator: root
+Created: 2018-07-09 11:25:59
+Attachments:
+
+"""
+
+
+def test_parse_history_response():
+    from RTIR import parse_history_response
+    parsed_history = parse_history_response(RAW_HISTORY)
+    assert parsed_history == {'ID': '80',
+                              'Content': 'Some\nMulti line\nContent',
+                              'Created': '2018-07-09 11:25:59',
+                              'Creator': 'root',
+                              'Description': 'Ticket created by root',
+                              'NewValue': 'some new value'}
